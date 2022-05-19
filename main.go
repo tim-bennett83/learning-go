950493bad07eee21025b72c25d6f7bde4aec2709
Timothy Bennett <timothy.bennett@rallyhealth.com>
2022-05-19 13:37:36 -0700

    adds JSON structures with happy-path test on fetching user info

--- main.go
+++ main.go
@@ -1,21 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
-func handleUserPosts() func(w http.ResponseWriter, r *http.Request) {
+type UserInfo struct {
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+type UserAndPostsInfo struct {
+	UserId   int      `json:"id"`
+	UserInfo UserInfo `json:"userInfo"`
+	Posts    []string `json:"posts"`
+}
+
+type UserPostsService interface {
+	GetUserInfo(userId int) (UserInfo, error)
+}
+
+func handleUserPosts(service UserPostsService) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		pathSegments := strings.Split(r.URL.Path, "/")
 		// there's certainly a better, more canonical method for getting the user ID from the path
-		userID := pathSegments[len(pathSegments)-1]
-		fmt.Fprintf(w, `{"userID": %q}`, userID)
+		userId, err := strconv.Atoi(pathSegments[len(pathSegments)-1])
+		if err != nil {
+			log.Printf("error when attempting to parse user ID from URI: %v", err)
+			// TODO: add response
+			return
+		}
+
+		userInfo, err := service.GetUserInfo(userId)
+
+		js, err := json.Marshal(UserAndPostsInfo{
+			userId,
+			userInfo,
+			make([]string, 0),
+		})
+		fmt.Fprint(w, string(js))
 	}
 }
 
 func main() {
-	http.HandleFunc("/v1/user-posts/", handleUserPosts())
-	http.ListenAndServe(":8081", nil)
+	http.HandleFunc("/v1/user-posts/", handleUserPosts(nil))
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
--- main_test.go
+++ main_test.go
@@ -1,25 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http/httptest"
 	"testing"
 )
 
+type UserPostsTestService struct{}
+
+func (s *UserPostsTestService) GetUserInfo(userId int) (info UserInfo, err error) {
+	return UserInfo{"Leanne Graham", "Bret", "Sincere@april.biz"}, nil
+}
+
 func TestMain(t *testing.T) {
-	t.Run("mew", func(t *testing.T) {
+	t.Run("receives basic user info", func(t *testing.T) {
 		response := httptest.NewRecorder()
-		handleUserPosts()(response, httptest.NewRequest("GET", "/v1/user-posts/42", nil))
+		handleUserPosts(&UserPostsTestService{})(response, httptest.NewRequest("GET", "/v1/user-posts/42", nil))
 		defer response.Result().Body.Close()
 
-		got, err := ioutil.ReadAll(response.Result().Body)
-		want := `{"userID": "42"}`
-
+		gotBytes, err := ioutil.ReadAll(response.Result().Body)
 		if err != nil {
 			t.Errorf("got error %v", err)
 		}
-		if string(got) != want {
-			t.Errorf("expected %v got %v", want, string(got))
+		var got UserAndPostsInfo
+		json.Unmarshal(gotBytes, &got)
+		want := UserAndPostsInfo{
+			42,
+			UserInfo{"Leanne Graham", "Bret", "Sincere@april.biz"},
+			make([]string, 0),
+		}
+
+		if got.UserId != want.UserId || got.UserInfo != want.UserInfo {
+			t.Errorf("expected %v got %v", want, got)
 		}
 	})
 }
